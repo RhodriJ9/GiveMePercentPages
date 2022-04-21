c689ac7add10b450dcce6be2c1ec3a1fe8fddf6e
RhodriJ9 <rhodri23@hotmail.co.uk>
2022-04-21 22:18:43 +0100

    init

---
+++ _nuxt/static/1650575906/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FGiveMePercentPages\u002F_nuxt\u002Fstatic\u002F1650575906",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FGiveMePercentPages\u002F",assetsPath:"\u002FGiveMePercentPages\u002F_nuxt\u002F",cdnURL:a}}}}(null));
